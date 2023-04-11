b910e2d5de305a4c8818ef15cd6f8885149a142a
RodrigoRamone7 <69323304+RodrigoRamone7@users.noreply.github.com>
2023-04-11 14:25:03 -0300

    Nova feature
    Nova feature que calcula o nível de atividade física automaticamente em níveis leve, moderado e intenso.
    Também corrigi a equação de calculo da taxa metabólica basal pois o calculo para a taxa feminina estava incorreto

--- taxabelatobilica.py
+++ taxabelatobilica.py
@@ -1,10 +1,28 @@
 def taxa_homem(p,a,i):
-    tmb = 66 + (13.7 * p) + (5 * a) - (6.8 * i)
-    print(tmb,"Kcal")
+    tmb = 66 + (13.8 * p) + (5 * a) - (6.8 * i)
+    print("Sua taxa metabólica basal é de", tmb,"Kcal")
+    nivel_atividade_H(tmb)
     
 def taxa_mulher(p,a,i):
-    tmb = 66 + (9.6 * p) + (1.8 * a) - (4.7 * i)
-    print(tmb,"Kcal")
+    tmb = 655 + (9.6 * p) + (1.9 * a) - (4.7 * i)
+    print("Sua taxa metabólica basal é de", tmb,"Kcal")
+    nivel_atividade_M(tmb)
+    
+def nivel_atividade_H(tmb):
+    a_leve = tmb*1.55
+    a_mod = tmb*1.78
+    a_int = tmb*2.10
+    print("Sua taxa metabólica basal em nível de atividade leve é de", a_leve,"Kcal")
+    print("Sua taxa metabólica basal em nível de atividade moderada é de", a_mod,"Kcal")
+    print("Sua taxa metabólica basal em nível de atividade intensa é de", a_int,"Kcal")
+    
+def nivel_atividade_M(tmb):
+    a_leve = tmb*1.56
+    a_mod = tmb*1.64
+    a_int = tmb*1.82
+    print("Sua taxa metabólica basal em nível de atividade leve é de", a_leve,"Kcal")
+    print("Sua taxa metabólica basal em nível de atividade moderada é de", a_mod,"Kcal")
+    print("Sua taxa metabólica basal em nível de atividade intensa é de", a_int,"Kcal")
     
 sex = str (input("Você é homem ou mulher? Responda com H para homem e M para mulher "))
 p = int (input("Qual o seu peso em Kg? "))
