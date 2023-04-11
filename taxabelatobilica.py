68ae8ad5cac586b5e623445b1b782fe4cdb93382
RodrigoRamone7 <69323304+RodrigoRamone7@users.noreply.github.com>
2023-04-11 15:46:44 -0300

    Formatação de números
    Números foram formatados para o padrão brasileiro e com 2 casas decimais após a vírgula

--- taxabelatobilica.py
+++ taxabelatobilica.py
@@ -1,28 +1,44 @@
 def taxa_homem(p,a,i):
     tmb = 66 + (13.8 * p) + (5 * a) - (6.8 * i)
-    print("Sua taxa metabólica basal é de", tmb,"Kcal")
+    tmbf = f'{tmb:_.2f}'
+    tmbf = tmbf.replace('.',',').replace('_','.')
+    print(f"Sua taxa metabólica basal é de {tmbf} Kcal")
     nivel_atividade_H(tmb)
     
 def taxa_mulher(p,a,i):
     tmb = 655 + (9.6 * p) + (1.9 * a) - (4.7 * i)
-    print("Sua taxa metabólica basal é de", tmb,"Kcal")
+    tmbf = f'{tmb:_.2f}'
+    tmbf = tmbf.replace('.',',').replace('_','.')
+    print(f"Sua taxa metabólica basal é de {tmbf} Kcal")
     nivel_atividade_M(tmb)
     
 def nivel_atividade_H(tmb):
     a_leve = tmb*1.55
     a_mod = tmb*1.78
     a_int = tmb*2.10
-    print("Sua taxa metabólica basal em nível de atividade leve é de", a_leve,"Kcal")
-    print("Sua taxa metabólica basal em nível de atividade moderada é de", a_mod,"Kcal")
-    print("Sua taxa metabólica basal em nível de atividade intensa é de", a_int,"Kcal")
+    a_levef = f'{a_leve:_.2f}'
+    a_modf = f'{a_mod:_.2f}'
+    a_intf = f'{a_int:_.2f}'
+    a_levef = a_levef.replace('.',',').replace('_','.')
+    a_modf = a_modf.replace('.',',').replace('_','.')
+    a_intf = a_intf.replace('.',',').replace('_','.')
+    print(f"Sua taxa metabólica basal em nível de atividade leve é de {a_levef} Kcal")
+    print(f"Sua taxa metabólica basal em nível de atividade moderada é de {a_modf} Kcal")
+    print(f"Sua taxa metabólica basal em nível de atividade intensa é de {a_intf} Kcal")
     
 def nivel_atividade_M(tmb):
     a_leve = tmb*1.56
     a_mod = tmb*1.64
     a_int = tmb*1.82
-    print("Sua taxa metabólica basal em nível de atividade leve é de", a_leve,"Kcal")
-    print("Sua taxa metabólica basal em nível de atividade moderada é de", a_mod,"Kcal")
-    print("Sua taxa metabólica basal em nível de atividade intensa é de", a_int,"Kcal")
+    a_levef = f'{a_leve:_.2f}'
+    a_modf = f'{a_mod:_.2f}'
+    a_intf = f'{a_int:_.2f}'
+    a_levef = a_levef.replace('.',',').replace('_','.')
+    a_modf = a_modf.replace('.',',').replace('_','.')
+    a_intf = a_intf.replace('.',',').replace('_','.')
+    print(f"Sua taxa metabólica basal em nível de atividade leve é de {a_levef} Kcal")
+    print(f"Sua taxa metabólica basal em nível de atividade moderada é de {a_modf} Kcal")
+    print(f"Sua taxa metabólica basal em nível de atividade intensa é de {a_intf} Kcal")
     
 sex = str (input("Você é homem ou mulher? Responda com H para homem e M para mulher "))
 p = int (input("Qual o seu peso em Kg? "))
